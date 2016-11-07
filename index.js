1f065353f0cd8221d32e0e84a5839fc62ee52b11
Laurence Berry <laurenceberry@GDS3930.lan>
2016-11-07 21:11:55 +0000

    Update content

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.intent('countOpportunities', {
     'utterances': [ '{how many new opportunities are there today}' ]
     },
     function(req, res) {
-        res.say('There are 3 new opportunities today').shouldEndSession(true);
+        res.say('There are 3 new opportunities today do you want to hear more?').shouldEndSession(false);
         
         return true;
     }
@@ -33,14 +33,14 @@ app.intent('listOpportunities', {
     'utterances': [ '{list opportunities|new opportunities|opportunities}' ]
     },
     function(req, res) {
-        res.say('OK, the latest 3 opportunities are: 1. Customer Journey and CRM Consultant from NHS Property Services in Digital specialists. 2. Business Analyst for Qualifications Wales’ Data Collection and Statistical Publications Project from Qualifications Wales in Digital specialists. 3. Design Manual for Roads and Bridges Agile Methodology Application from Highways England in Digital outcomes. Do you want to hear more from opportunity 1, 2 or 3?').shouldEndSession(true);
+        res.say('OK, the latest 3 opportunities are: 1. Customer Journey and CRM Consultant from NHS Property Services in Digital specialists. 2. Business Analyst for Qualifications Wales’ Data Collection and Statistical Publications Project from Qualifications Wales in Digital specialists. 3. Design Manual for Roads and Bridges Agile Methodology Application from Highways England in Digital outcomes. Do you want to hear more from opportunity 1, 2 or 3?').shouldEndSession(false);
         return true;
     }
 );
 
 app.intent('readOpportunity', {
     'slots': {},
-    'utterances': [ '{read four|open opportunity four}' ]
+    'utterances': [ '{read three|open opportunity three}' ]
     },
     function(req, res) {
         res.say('Design Manual for Roads and Bridges Agile Methodology Application from Highways England located in South West England. This project will have an initial discovery phase to allow for full definition of user stories and system requirements to be determined. The overall desired outcome of the project will be a working pilot system and a Highways England team capable of applying agile practices to their backlog of tasks. This opportunity closes Monday 14 November 2016. Visit the Digital Marketplace to apply.').shouldEndSession(true);
