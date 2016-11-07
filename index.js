72b1153c2ec6ec094f18ed3a1478a09b3d023840
Laurence Berry <laurenceberry@GDS3930.lan>
2016-11-07 20:32:40 +0000

    Adds test intents

--- index.js
+++ index.js
@@ -17,19 +17,36 @@ app.error = function( exception, request, response ) {
 	response.say( 'Sorry an error occured ' + error.message);
 };
 
-app.intent('sayNumber',
-  {
-    "slots":{"number":"NUMBER"}
-	,"utterances":[ 
-		"say the number {1-100|number}",
-		"give me the number {1-100|number}",
-		"tell me the number {1-100|number}",
-		"I want to hear you say the number {1-100|number}"]
-  },
-  function(request,response) {
-    var number = request.slot('number');
-    response.say("You asked for the number "+number);
-  }
+app.intent('countOpportunities', {
+    'slots': {},
+    'utterances': [ '{how many new opportunities are there today}' ]
+    },
+    function(req, res) {
+        res.say('There are 4 new opportunities today').shouldEndSession(true);
+        
+        return true;
+    }
+);
+
+app.intent('listOpportunities', {
+    'slots': {},
+    'utterances': [ '{list opportunities|new opportunities|opportunities}' ]
+    },
+    function(req, res) {
+        res.say('OK, the latest 4 opportunities are: 1. Customer Journey and CRM Consultant from NHS Property Services in Digital specialists. 2. Business Analyst for Qualifications Wales’ Data Collection and Statistical Publications Project from Qualifications Wales in Digital specialists. 3. Design Manual for Roads and Bridges (DMRB) Agile Methodology Application from Highways England in Digital outcomes and 4. ITS Cloud Application Developer (8) from Driver and Vehicle licensing Agency in Digital specialists. Do you want to hear more from opportunity 1, 2, 3 or 4?').shouldEndSession(true);
+        return true;
+    }
+);
+
+app.intent('readOpportunity', {
+    'slots': {},
+    'utterances': [ '{read 4|open opportunity 4}' ]
+    },
+    function(req, res) {
+        res.say('Customer Journey and CRM Consultant from NHS Property Services located in London. DVLA Software Engineering Capability is looking for highly skilled developers to undertake the development and support of DVLA’s applications both internal and public/customer facing. This opportunity closes Monday 14 November 2016. Visit the Digital Marketplace to apply.').shouldEndSession(true);
+        return true;
+    }
 );
 
 module.exports = app;
+
