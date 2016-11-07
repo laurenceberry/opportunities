0fc441a49bd9af61c81a1cca75c7b217236b3d54
Laurence Berry <laurenceberry@GDS3930.lan>
2016-11-07 20:52:42 +0000

    Test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = new alexa.app( 'opportunities' );
 
 
 app.launch( function( request, response ) {
-	response.say( 'Welcome Marketplace' ).reprompt( 'You can ask me to list opportunities' ).shouldEndSession( false );
+	response.say( 'Welcome to Marketplace' ).reprompt( 'You can ask me to list opportunities' ).shouldEndSession( false );
 } );
 
 
@@ -24,26 +24,6 @@ app.intent('countOpportunities', {
     function(req, res) {
         res.say('There are 4 new opportunities today').shouldEndSession(true);
         
-        return true;
-    }
-);
-
-app.intent('listOpportunities', {
-    'slots': {},
-    'utterances': [ '{list opportunities|new opportunities|opportunities}' ]
-    },
-    function(req, res) {
-        res.say('OK, the latest 4 opportunities are: 1. Customer Journey and CRM Consultant from NHS Property Services in Digital specialists. 2. Business Analyst for Qualifications Wales’ Data Collection and Statistical Publications Project from Qualifications Wales in Digital specialists. 3. Design Manual for Roads and Bridges (DMRB) Agile Methodology Application from Highways England in Digital outcomes and 4. ITS Cloud Application Developer (8) from Driver and Vehicle licensing Agency in Digital specialists. Do you want to hear more from opportunity 1, 2, 3 or 4?').shouldEndSession(true);
-        return true;
-    }
-);
-
-app.intent('readOpportunity', {
-    'slots': {},
-    'utterances': [ '{read four|open opportunity four}' ]
-    },
-    function(req, res) {
-        res.say('Customer Journey and CRM Consultant from NHS Property Services located in London. DVLA Software Engineering Capability is looking for highly skilled developers to undertake the development and support of DVLA’s applications both internal and public/customer facing. This opportunity closes Monday 14 November 2016. Visit the Digital Marketplace to apply.').shouldEndSession(true);
         return true;
     }
 );
